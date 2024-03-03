c932738cf7785c856eadb407a0eff4799250f594
deadpeopleseeme <122888042+deadpeopleseeme@users.noreply.github.com>
2024-03-03 10:12:24 +0100

    Initial commit

---
+++ ListsToTextReader.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Diagnostics;
+
+namespace Module13
+{
+    public class ListsToTextReader
+    {
+        public string PathToFile { get; set; }
+        public List<string> TestList { get; set; }
+        public LinkedList<string> TestLinkedList { get; set; }
+
+        public bool IsPathOK { get; private set; }
+
+        public StreamReader Reader { get; set; }
+
+        public void TextToListReader()
+        {
+
+            Reader = new(PathToFile);
+            string? line;
+            while ((line = Reader.ReadLine()) != null)
+            {
+                TestList.Add(line);
+            }
+        }
+
+        public void TextToLinkedListReader()
+        {
+
+            Reader = new(PathToFile);
+            string? line;
+            while ((line = Reader.ReadLine()) != null)
+            {
+                TestLinkedList.AddLast(line);
+            }
+        }
+
+        public ListsToTextReader(string path)
+        {
+            PathToFile = path;
+            try
+            {
+                Reader = new(PathToFile);
+                IsPathOK = true;
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+                IsPathOK = false;
+            }
+            TestList = [];
+            TestLinkedList = [];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet;
+using BenchmarkDotNet.Running;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Diagnostics;
+
+namespace Module13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListsToTextReader myReader = new(path: "C:\\Users\\Eddy\\Desktop\\Text1.txt");
+
+            if (myReader.IsPathOK)
+            {
+                long listSumElapsedTime = 0;
+                long linkedListSumElapsedTime = 0;
+
+                Console.WriteLine($"Сначала измеряем время вставки для обычного List<>\n");
+
+                var timer = new Stopwatch();
+                timer.Start();
+                
+                for (int i = 0; i < 100; i++)
+                {
+                    timer.Restart();
+                    myReader.TextToListReader();
+                    timer.Stop();
+                    listSumElapsedTime += timer.ElapsedMilliseconds;
+                }
+                Console.WriteLine($"Суммарное время за 100 проходов для List<>: {listSumElapsedTime}\n");
+
+                Console.WriteLine($"Теперь для LinkedList<>\n");
+
+                timer.Start();
+
+                for (int i = 0; i < 100; i++)
+                {
+                    timer.Restart();
+                    myReader.TextToLinkedListReader();
+                    timer.Stop();
+                    linkedListSumElapsedTime += timer.ElapsedMilliseconds;
+                }
+                Console.WriteLine($"Суммарное время за 100 проходов для LinkedList<>: {linkedListSumElapsedTime}\n");
+            }
+        }
+    }
+}
